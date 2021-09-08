6e77a46ddccc4d3777fad15e359a4f4c97b9b2ca
padmini v <padmini.v@perfios.com>
2021-09-08 16:48:56 +0530

    ADDED: analyseMood method to return the mood string

---
+++ src/test/java/com/bridgelabz/moodanalyser/MoodAnalyserTest.java
@@ -0,0 +1,31 @@
+package com.bridgelabz.moodanalyser;
+
+import static org.junit.Assert.*;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyserTest 
+{
+
+	@Test
+	public void givenMessage_WhenSad_ShouldReturnSad() 
+	{
+		
+		MoodAnalyser moodAnalyser=new MoodAnalyser();
+		String mood= moodAnalyser.analyseMood("This is a happy message");
+		Assert.assertThat(mood, CoreMatchers.is("HAPPY"));
+	}
+	
+	@Test
+	public void givenMessage_WhenHappy_ShouldReturnHappy() 
+	{
+		
+		MoodAnalyser moodAnalyser=new MoodAnalyser();
+		String mood= moodAnalyser.analyseMood("This is a sad message");
+		Assert.assertThat(mood, CoreMatchers.is("SAD"));
+	}
+	
+
+}
